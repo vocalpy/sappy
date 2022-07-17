a56b657c59ae613b196f0807c6cd870bc2f8ee5f
David Nicholson <nickledave@users.noreply.github.com>
2022-07-17 18:05:54 -0500

    Rename `plot` module functions
    to remove the word `plot` from names,
    so we can just write `sappy.plot.spectral_derivs`
    instead of `sappy.spectral_derivs_plot`.
    Requires importing `plot` directly at package root,
    instead of importing functions from plot one-by-one.

--- src/sappy/__init__.py
+++ src/sappy/__init__.py
@@ -1,7 +1,4 @@
+from . import plot
 from .songfeatures import *
-
-from .plot import spectral_derivs_plot, plot_over_spec, similarity_plot
-
 from .similarity import similarity
-
 from .utils import normalize_features
--- src/sappy/plot.py
+++ src/sappy/plot.py
@@ -1,20 +1,18 @@
-"""Plotting function for birdsonganalysis."""
-
+"""Plotting functions for ``sappy``."""
 import numpy as np
 import seaborn as sns
 import matplotlib.patches as p
 import matplotlib.pyplot as plt
 
-from .songfeatures import spectral_derivs
+from . import songfeatures
 
 
-def spectral_derivs_plot(spec_der, contrast=0.1, ax=None, freq_range=None,
-                         ov_params=None):
-    """
-    Plot the spectral derivatives of a song in a grey scale.
+def spectral_derivs(spec_der, contrast=0.1, ax=None, freq_range=None,
+                    ov_params=None):
+    """Plot spectral derivative in greyscale.
 
     spec_der - The spectral derivatives of the song (computed with
-               `spectral_derivs`) or the song itself
+               ``sappy.songfeatures.spectral_derivs``) or the song itself
     contrast - The contrast of the plot
     ax - The matplotlib axis where the plot must be drawn, if None, a new axis
          is created
@@ -23,7 +21,7 @@ def spectral_derivs_plot(spec_der, contrast=0.1, ax=None, freq_range=None,
     ov_params - The Parameters to override, passed to `spectral_derivs`
     """
     if spec_der.ndim == 1:
-        spec_der = spectral_derivs(spec_der, freq_range, ov_params)
+        spec_der = songfeatures.spectral_derivs(spec_der, freq_range, ov_params)
     ax = sns.heatmap(spec_der.T, yticklabels=100, xticklabels=100,
                      vmin=-contrast, vmax=contrast, ax=ax, cmap='Greys',
                      cbar=False)
@@ -31,15 +29,14 @@ def spectral_derivs_plot(spec_der, contrast=0.1, ax=None, freq_range=None,
     return ax
 
 
-def plot_over_spec(data, ax, freq_range=256, **plot_params):
-    """
-    Plot the feature over a spectral derivatives plot.
+def features_over_spec(data, ax, freq_range=256, **plot_params):
+    """Plot an acoustic feature over a spectral derivatives plot.
 
-    The data are first normalized then rescale to fit the ylim of the axis.
+    The data are first normalized, then rescaled to fit the ylim of the axis.
     """
     # Normalize the data so that they fit in the graph
     ndata = data / (np.nanmax(data) - np.nanmin(data))
-    # We plot -ndata because the yaxis is inverted (see `spectral_derivs_plot`)
+    # We plot -ndata because the yaxis is inverted (see ``sappy.plot.spectral_derivs``)
     # We take for abscisse axis 95% of freq_range
     # We rescale the data so that they take 75% of the graph
     ax.plot(95/100 * freq_range - 75/100 * freq_range
@@ -47,18 +44,17 @@ def plot_over_spec(data, ax, freq_range=256, **plot_params):
     return ax
 
 
-def similarity_plot(sim, song, refsong):
-    """Do a similarity plot with the result of `bsa.similarity`."""
+def similarity(sim, song, refsong):
+    """Plot the result of ``sappy.similarity``."""
     fig, ax = plt.subplots(2, 2, figsize=(13, 13),
                            gridspec_kw={'width_ratios': [1, 4],
                                         'height_ratios': [1, 4]})
     ax[0, 0].axis('off')
-    sds = spectral_derivs(song)
-    sdr = spectral_derivs(refsong)
-    ax[0, 1] = spectral_derivs_plot(sds, 0.05, ax[0, 1])
+    sds = songfeatures.spectral_derivs(song)
+    sdr = songfeatures.spectral_derivs(refsong)
+    ax[0, 1] = spectral_derivs(sds, 0.05, ax[0, 1])
     ax[0, 1].set_title('Song')
-    ax[1, 0] = spectral_derivs_plot(np.flip(sdr.T, 1), 0.05,
-                                    ax[1, 0])
+    ax[1, 0] = spectral_derivs(np.flip(sdr.T, 1), 0.05, ax[1, 0])
     ax[1, 0].set_title('Reference Song')
     ax[1, 1] = sns.heatmap(sim['glob_matrix'], ax=ax[1, 1], cbar=False,
                            vmin=0, vmax=1)
